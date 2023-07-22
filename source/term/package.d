78f11baccfc09cabb043b096293122282e1f4052
Ikey Doherty <ikey@serpentos.com>
2023-07-22 23:07:09 +0100

    term: Fix the copyright range
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/term/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * term
+ *
+ * Module level imports
+ *
+ * Authors: Copyright © 2023 Serpent OS Developers
+ * License: Zlib
+ */
+
+module term;
