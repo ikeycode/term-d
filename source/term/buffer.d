38597b78e31435d5602057ed4ef2e2e5ef630b33
Ikey Doherty <ikey@serpentos.com>
2023-07-23 00:41:11 +0100

    term: Add Buffer
    This is a malloc based buffer that will only grow upwards with a scaling
    factor to prevent too much noise in terms of allocations which would
    lead to nasty frame drops + repaints.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/term/buffer.d
@@ -0,0 +1,142 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * term.buffer
+ *
+ * Provides a simple, managed terminal buffer
+ *
+ * Authors: Serpent OS Developers
+ * License: Zlib
+ * Copyright: © 2023 Serpent OS Developers
+ */
+
+module term.buffer;
+
+@safe:
+
+public import term.cell;
+
+import std.experimental.allocator;
+import std.experimental.allocator.mallocator;
+
+/// The buffer grows twice as big as the input request to prevent further allocations
+const growthFactor = 2;
+
+/** 
+ * A display buffer to manage a grid of cells, handling
+ * allocation internally.
+ *
+ * It will happily grow, but will never shrink.
+ */
+public final class Buffer
+{
+
+    @disable this();
+
+    /** 
+     * Construct a new Buffer
+     *
+     * Params:
+     *   width = Required width
+     *   height = Required height
+     */
+    this(ulong width, ulong height) @nogc
+    {
+        this._width = width;
+        this._height = height;
+
+        memory = Mallocator.instance.makeArray!(Cell)(width * height * growthFactor, Cell.init);
+        assert(memory !is null, "Fatal allocation error");
+        allocatedWidth = width * growthFactor;
+        allocatedHeight = height * growthFactor;
+        allocatedSize = allocatedHeight * allocatedWidth;
+    }
+
+    /** 
+     * Returns: Usable width
+     */
+    pure @property ulong width() => _width;
+
+    /** 
+     * Returns: Usable height
+     */
+    pure @property ulong height() => _height;
+
+    /** 
+     * Restricted window into the currently visible buffer
+     */
+    auto opSlice() @nogc => memory[0 .. (_width * _height)];
+
+    ~this() @nogc
+    {
+        release();
+    }
+
+    /** 
+     * Request a resize of the buffer
+     * Params:
+     *   width = New width for the buffer
+     *   height = New height for the buffer
+     */
+    void resize(ulong width, ulong height) @nogc
+    {
+        // No need to shrink the array
+        _width = width;
+        _height = height;
+
+        auto newSize = width * height;
+
+        if (newSize < allocatedSize)
+        {
+            return;
+        }
+
+        newSize *= growthFactor;
+        const delta = newSize - allocatedSize;
+        const resized = () @trusted {
+            return Mallocator.instance.expandArray(memory, delta);
+        }();
+        if (!resized)
+        {
+            assert(0, "Growth failure");
+        }
+        allocatedHeight = height * growthFactor;
+        allocatedWidth = width * growthFactor;
+        allocatedSize = allocatedWidth * allocatedHeight;
+    }
+
+    /** 
+     * Release all resources
+     */
+    void release() @nogc
+    {
+        if (allocatedHeight < 0)
+            return;
+
+        // free the memory
+        allocatedHeight = 0;
+        () @trusted { Mallocator.instance.dispose(memory); }();
+    }
+
+private:
+
+    ulong _width;
+    ulong _height;
+    Cell[] memory;
+
+    ulong allocatedWidth;
+    ulong allocatedHeight;
+    ulong allocatedSize;
+}
+
+@safe unittest
+{
+    auto b = new Buffer(1, 1);
+    scope (exit)
+        b.release();
+    b.resize(5, 5);
+}
